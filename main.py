07975273755785b7790ce73a618a13940ca222b9
Vivyen Vu <pokeviv@gmail.com>
2022-11-19 15:33:07 -0500

    trying different things to accept large dataset

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pandas.plotting import scatter_matrix
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-#from matplotlib import pyplot
 from sklearn import linear_model
 
 df = pd.read_csv('sloth_data.csv')
@@ -16,15 +15,9 @@ print(df)
 print('This is df_chars')
 print(df_chars)
 
-#sns.heatmap(df_chars, annot=True, cmap='Blues');
-#plt.ylabel('Actual label')
-#plt.xlabel('Predicted label')
-#plt.show()
-
 scatter_matrix(df)
 plt.show()
 
-#print(df)
 df.hist()
 plt.show()
 
@@ -50,9 +43,9 @@ plt.show()
 
 df_chars = df_chars[['claw_length_cm', 'weight_kg', 'sub_specie']]
 print(list(df_chars.columns.values))
-mylog_model = linear_model.LogisticRegression(max_iter=250)
-y = df_chars.values[:200, 2]
-x = df_chars.values[:200,0:2]
+mylog_model = linear_model.LogisticRegression(tol = 0.1)
+y = df_chars.values[:, 2]
+x = df_chars.values[:,0:2]
 
 mylog_model.fit(x, y)
 
