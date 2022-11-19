23115dc8933023f2d04ea3da3a3b9c5b47f2d368
Vivyen Vu <pokeviv@gmail.com>
2022-11-19 15:30:12 -0500

    train model on only two parameters: claw_length_cm and weight_kg

--- main.py
+++ main.py
@@ -38,15 +38,25 @@ critically_endangered = df.loc[df['endangered']=='critically_endangered', 'size_
 plt.boxplot([least_concern, vulnerable, critically_endangered], labels=['Least concern','Vulnerable', 'Critically Endangered'])
 plt.show()
 
-df_chars = df_chars[['claw_length_cm', 'size_cm', 'tail_length_cm', 'weight_kg', 'sub_specie']]
+#df_chars = df_chars[['claw_length_cm', 'size_cm', 'tail_length_cm', 'weight_kg', 'sub_specie']]
+#print(list(df_chars.columns.values))
+#mylog_model = linear_model.LogisticRegression(max_iter=250)
+#y = df_chars.values[:200, 4]
+#x = df_chars.values[:200,0:4]
+
+#mylog_model.fit(x, y)
+
+#print(mylog_model.predict([[9.2, 64.7, -.01, 6.6]]))
+
+df_chars = df_chars[['claw_length_cm', 'weight_kg', 'sub_specie']]
 print(list(df_chars.columns.values))
 mylog_model = linear_model.LogisticRegression(max_iter=250)
-y = df_chars.values[:200, 4]
-x = df_chars.values[:200,0:4]
+y = df_chars.values[:200, 2]
+x = df_chars.values[:200,0:2]
 
 mylog_model.fit(x, y)
 
-print(mylog_model.predict([[9.2,64.7, -.01, 6.6]]))
+print(mylog_model.predict([[8.596, 3.487]]))
 
 
 
