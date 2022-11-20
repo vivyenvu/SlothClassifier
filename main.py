01c135cd111934644536304d197d17e951e806a5
Vivyen Vu <pokeviv@gmail.com>
2022-11-19 21:34:34 -0500

    4 attributes is more accurate than 2. Let me check if max_iter makes a difference

--- main.py
+++ main.py
@@ -31,24 +31,24 @@ dfE = df.loc[endangered_filter, :]
 #plt.boxplot([least_concern, vulnerable, critically_endangered], labels=['Least concern','Vulnerable', 'Critically Endangered'])
 #plt.show()
 
-#df_chars = df_chars[['claw_length_cm', 'size_cm', 'tail_length_cm', 'weight_kg', 'sub_specie']]
-#print(list(df_chars.columns.values))
-#mylog_model = linear_model.LogisticRegression(max_iter=250)
-#y = df_chars.values[:200, 4]
-#x = df_chars.values[:200,0:4]
-
-#mylog_model.fit(x, y)
-
-#print(mylog_model.predict([[9.2, 64.7, -.01, 6.6]]))
-
-df_chars = df_chars[['claw_length_cm', 'weight_kg', 'sub_specie']]
-mylog_model = linear_model.LogisticRegression(tol = 0.1, max_iter=5000)
-y = df_chars.values[:, 2]
-x = df_chars.values[:,0:2]
+df_chars = df_chars[['claw_length_cm', 'size_cm', 'tail_length_cm', 'weight_kg', 'sub_specie']]
+print(list(df_chars.columns.values))
+mylog_model = linear_model.LogisticRegression(max_iter=250)
+y = df_chars.values[:200, 4]
+x = df_chars.values[:200,0:4]
 
 mylog_model.fit(x, y)
 
-print(mylog_model.predict([[5.856, 5.598]]))
+print(mylog_model.predict([[6.514, 64.194, 5.8, 6.635]]))
+
+df_chars = df_chars[['claw_length_cm', 'weight_kg', 'sub_specie']]
+mylog_model2 = linear_model.LogisticRegression(max_iter=5000)
+y = df_chars.values[:, 2]
+x = df_chars.values[:,0:2]
+
+mylog_model2.fit(x, y)
+
+print(mylog_model2.predict([[6.514, 6.635]]))
 
 
 
